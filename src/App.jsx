5f5186e5b0141868d9af56d319ff8fa1046309fb
GOGOJO <joshualu76@gmail.com>
2025-05-28 08:57:12 -0700

    added favouriting function

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,43 @@ import { useState, useEffect } from "react";
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 import Search from "./components/Search";
+import AddFavourite from "./components/AddFavourite";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [favourites, setFavourites] = useState([]);
   const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6416692f`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    if(responseJson.Search){
+    if (responseJson.Search) {
       setMovies(responseJson.Search);
     }
-    
   };
 
   useEffect(() => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  const addFavouriteMovie = (movie) => {
+    //creates a copy of the list using ...favourite and add movie to it the set it the our stateful variable
+    const newFavouriteList = [...favourites,movie];
+    setFavourites(newFavouriteList);
+  }
   //default search value
   return (
     <div className="container-fluid movie-app">
-     <div className="row d-flex align-items-center mt-4 mb-4">
-        <MovieListHeading heading='Movies'/>
-        <Search searchValue = {searchValue} setSearchValue = {setSearchValue} />
-      </div> 
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList movies={movies} favouriteComponent={AddFavourite} handleFavouriteClicks={addFavouriteMovie}/>
       </div>
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,23 @@
+
+
+
 const MovieList = (props) => {
+
+  const FavouriteComponent = props.favouriteComponent;
+
   return (
     <>
       {props.movies && props.movies.length > 0 ? (
         props.movies.map((movie, index) => (
-          <div className="col-auto" key={movie.imdbID || index}>
+          <div className="image-container col-auto" key={movie.imdbID || index}>
             <img
               src={movie.Poster}
               alt={movie.Title}
               style={{ height: "300px", width: "200px", objectFit: "cover" }}
             />
+            <div onClick = {props.handleFavouriteClick} className="overlay d-flex align-items-center justify-content-center">
+              <FavouriteComponent/>
+            </div>
           </div>
         ))
       ) : (
